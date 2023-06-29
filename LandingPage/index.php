79d016df165eead9c204822d8f570d04f3b7c8af
Gabriel Siriani <50271636+Gn112@users.noreply.github.com>
2023-06-29 11:34:49 -0300

    Add files via upload

---
+++ LandingPage/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BuffEats</title>
+    <link rel="icon" type="image/x-icon">
+    <link rel="stylesheet" href="landingpage.css">
+</head>
+
+<body>
+    <main>
+            <div class="container">
+                <span class="landing_title">Vai rolar festinha?</span>
+                <span class="landing_title">Chama no Buff<a>Eats</a>!</span>
+                <span class="landing_subtitle">Com BuffEats você pode escolher buffets<br>de qualidade e com preços justos.</span>
+                <div class="row"">
+                <button type="submit" name="submit" id="recuperar" class="small_button">
+                Tenho interesse!
+                    </button>
+                </div>
+        </div>
+    </main>
+</body>
