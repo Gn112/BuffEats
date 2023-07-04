d9c2d313ce2a6b27fa1ee753ab2428123b981796
Edu0103 <99186559+Edu0103@users.noreply.github.com>
2023-07-03 23:00:44 -0300

    Add files via upload

--- src/Login/conexaoLogin.php
+++ src/Login/conexaoLogin.php
@@ -3,8 +3,8 @@ ini_set('default_charset', 'utf-8');
 ?>
 
 <?php
-$servername = "34.95.153.229";
-$database = "buffeat";
+$servername = "34.136.11.187";
+$database = "Buffeats";
 $username = "root";
 $password = "COTemig123";
 // Create connection
@@ -15,8 +15,33 @@ if (!$conn) {
 }
 echo "Connected successfully";
 
-$email = $_POST['email'];
-$senha = $_POST['senha'];
 
-$sql = "SELECT * FROM CADASTRO_CLIENTE WHERE email = '$email' AND senha = '$senha'";
-$result = $conn->query($sql);
+
+
+if (isset($_POST['submit'])) {
+    // Recupere os valores dos campos do formulário
+    $email = (string)$_POST["email"];
+    $senha = (string)$_POST["senha"];
+   
+    $query = "SELECT * FROM CADASTRO_CLIENTE WHERE email = '$email'";
+    $resultado = mysqli_query($conn, $query);
+    
+    // Verifica se a consulta retornou algum resultado
+    if (mysqli_num_rows($resultado) == 1) {
+        // Obtém os dados do usuário do resultado da consulta
+        $dadosUsuario = mysqli_fetch_assoc($resultado);
+    }
+    
+        // Verifica se a senha digitada está correta
+        if (password_verify($password, $dadosUsuario['senha']))
+        {
+            echo "sexo" ;
+        }
+    
+    }
+
+
+
+
+
+?>
