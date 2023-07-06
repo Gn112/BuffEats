c912497419c35eed216431fcadacc294de3959c3
Edu0103 <99186559+Edu0103@users.noreply.github.com>
2023-07-06 07:58:45 -0300

    Update conexaoLogin.php

--- src/Login/conexaoLogin.php
+++ src/Login/conexaoLogin.php
@@ -18,27 +18,7 @@ echo "Connected successfully";
 
 
 
-if (isset($_POST['submit'])) {
-    // Recupere os valores dos campos do formulário
-    $email = (string)$_POST["email"];
-    $senha = (string)$_POST["senha"];
-   
-    $query = "SELECT * FROM CADASTRO_CLIENTE WHERE email = '$email'";
-    $resultado = mysqli_query($conn, $query);
-    
-    // Verifica se a consulta retornou algum resultado
-    if (mysqli_num_rows($resultado) == 1) {
-        // Obtém os dados do usuário do resultado da consulta
-        $dadosUsuario = mysqli_fetch_assoc($resultado);
-    }
-    
-        // Verifica se a senha digitada está correta
-        if (password_verify($password, $dadosUsuario['senha']))
-        {
-            echo "sexo" ;
-        }
-    
-    }
+}
 
 
 
