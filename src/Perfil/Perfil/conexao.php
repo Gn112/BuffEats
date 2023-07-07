90ab32f2f518093fcd1be3ea660d5785ae896369
Duda Carvalho <105288202+dudaccarvalho@users.noreply.github.com>
2023-07-07 18:45:12 -0300

    Add files via upload

---
+++ src/Perfil/Perfil/conexao.php
@@ -0,0 +1,8 @@
+<?php
+$servidor = "34.136.11.187";
+$usuario = "root";
+$senha = "COTemig123";
+$dbname = "Buffeats";
+
+//Criar a conexao
+$conn = mysqli_connect($servidor, $usuario, $senha, $dbname); ?>
