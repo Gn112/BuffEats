678c2b3a2136767cb5132bfb107ec4d1adf01970
Gabriel Siriani <gabrielsiriani19@gmail.com>
2023-09-04 20:35:23 -0300

    50% da Tela perfil cliente e pacotes NPM

--- src/home/Cliente/perfil-cliente.php
+++ src/home/Cliente/perfil-cliente.php
@@ -2,14 +2,15 @@
 
 <input type="submit" name="logoff" value="Deslogar">
 <?php
-if(isset($_POST['logoff'])){
+if (isset($_POST['logoff'])) {
     session_destroy();
     header('Location: ../../Login/index.php');
 }
 ?> -->
 
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-br" class="sm:scroll-smooth">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,8 +18,28 @@ if(isset($_POST['logoff'])){
     <title>Perfil | BuffEats</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
-<header>
 
+<body class="min-h-screen bg-cinzafundo">
+    <header class="drop-shadow-xl text-fontecinza bg-branco sticky top-0 z-10">
+        <section class="max-w-full mx-auto p-4 flex justify-around itens-center">
+            <a href="#">
+                <img src="../../globalAssets/img/logo.png">
+            </a>
+
+            <div>
+                <button id="hamburger-button" class="text-3xl md:hidden cursor-pointer focus:outline">
+                    &#9776;
+                </button>
+                <nav class="hidden md:block space-x-3 text-xl" aria-label="main">
+                    <a href="#" class="text-fontecinza text-md">Home</a>
+                    <a href="#" class="text-fontecinza text-md">Quem Somos</a>
+                    <a class="signIn" id="criar">criar conta</a>
+                    <a class="redBtn" id="entrar">Entrar</a>
+                </nav>
+            </div>
+        </section>
+        <section id="mobile-menu"></section>
+    </header>
 </body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 
-/* Content aplica o Tailwind para todos os arquivos 
-html, js e php */
 module.exports = {
-  content: ['"*/*.{html,js,php}"'],
+  content: ["./src/home/Cliente/**.{html,js,php}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vermelho: "#BD2323",
+        hoververmelho: "#be4d4d",
+        branco: "#ffffff",
+        fontecinza: "#463B3B",
+        hoverfontecinza: "#000000",
+        cinzaclaro: "#F8F8F8",
+        cinzafundo: "#EEEEEE",
+        shadowcaixagrande: "0rem .3125rem .625rem .125rem rgba(0, 0, 0, 0.2)",
+        shadowcaixainput: "0rem .25rem .625rem rgba(0, 0, 0, 0.15)",
+      },
+      fontFamily: {
+        outfit: ['Outfit','Arial'],
+      }
+    },
   },
   plugins: [],
-}
+};
