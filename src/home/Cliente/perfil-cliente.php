b19f5775906653a7a4368fd7f12744bbed1ae58c
Gabriel Siriani <gabrielsiriani19@gmail.com>
2023-09-04 17:20:38 -0300

    Pacotes NPM e Configuração do Tailwind
    Pacotes NPM e Configuração do Tailwind.
    
    Espero que não tenha bug :))

--- src/home/Cliente/perfil-cliente.php
+++ src/home/Cliente/perfil-cliente.php
@@ -1,4 +1,4 @@
-CLIENTE
+<!-- CLIENTE
 
 <input type="submit" name="logoff" value="Deslogar">
 <?php
@@ -6,4 +6,19 @@ if(isset($_POST['logoff'])){
     session_destroy();
     header('Location: ../../Login/index.php');
 }
-?>
+?> -->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfil | BuffEats</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+/* Content aplica o Tailwind para todos os arquivos 
+html, js e php */
+module.exports = {
+  content: ['"*/*.{html,js,php}"'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
